efbebe0407f6f0ba41d4e0023ee231b824ecdaed
Goh Wei Wen <goweiwen@gmail.com>
2018-10-20 12:02:14 -0700

    Add Sidebar and Navbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,70 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  main
+    nav#toolbar.navbar.is-hidden-desktop
+      .navbar-brand
+        button.button.is-white.navbar-burger(@click="isSidebarHidden = !isSidebarHidden")
+          span
+          span
+          span
+    sidebar#sidebar(:class="{ hidden: isSidebarHidden }" @route="isSidebarHidden = true")
+    transition-group(name='fade' mode='out-in')
+      router-view#main(key='router-view')
 </template>
-<style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+<script>
+import Sidebar from "./components/Sidebar";
+
+export default {
+  name: "app",
+  components: { Sidebar },
+  data() {
+    return { isSidebarHidden: true };
+  }
+};
+</script>
+
+<style lang="stylus">
+w = 200px;
+
+.has-navbar-fixed-top {
+  padding-top: 3.25rem;
+}
+
+#toolbar {
+  z-index: 2;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+
+  .navbar-burger {
+    margin-left: 0;
+  }
+}
+
+#sidebar {
+  position: absolute;
+  width: 100vw;
+  z-index: 1;
+  padding: 16px;
+  overflow: hidden;
+  transition: all 0.3s;
+}
+
+@media screen and (max-width: 1023px) {
+  #sidebar.hidden {
+    transform: translateY(-100%);
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  #sidebar {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: w;
+    height: 100%;
+  }
+
+  #main {
+    margin-left: w;
+  }
+}
 </style>
---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  aside.menu#sidebar
+    p.menu-label General
+    ul.menu-list
+      li
+        router-link(to="/" @click.native="click") Home
+      li
+        router-link(to="/about" @click.native="click") About
+      li
+        a(@click="confirmLogout" @click.native="click") Log Out
+</template>
+
+<script>
+export default {
+  name: "sidebar",
+
+  methods: {
+    click() {
+      this.$emit('route')
+    },
+    confirmLogout() {
+      this.$dialog.confirm({
+        title: 'Logging out',
+        message: 'Are you sure you want to log out?',
+        cancelText: 'Cancel',
+        confirmText: 'Log Out',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: this.logout,
+      })
+    },
+    logout() {
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+#sidebar {
+  background-color: #ffffff;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.1);
+}
+</style>
