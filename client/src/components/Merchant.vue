d0d77de5cc9ef8e99620574edc1f3b64146cbe19
Goh Wei Wen <goweiwen@gmail.com>
2018-10-21 10:17:54 -0700

    Add form

--- client/src/components/Merchant.vue
+++ client/src/components/Merchant.vue
@@ -11,12 +11,34 @@
     b-modal(:active.sync="isModalActive" has-modal-card)
       .modal-card
         header.modal-card-head
-          p.modal-card-title Credit something
+          p.modal-card-title Don't worry!
         section.modal-card-body
-          p words words words
+          p(style="margin-bottom: 0.5em") Thomas,
+          p
+            | It looks like this purchase exceeds your credit limit.
+            | We can extend your credit limit to $1,000 with deferred interest for 12 months.
+          p(style="margin-top: 0.5em") Would you like that?
         footer.modal-card-foot
-          button.button.is-primary(@click="acceptTerms") OK
+          button.button.is-primary(@click="acceptTerms") Sure!
           button.button(@click="isModalActive = false") No, thank you
+
+    b-modal(:active.sync="isModalFormActive" has-modal-card)
+      .modal-card
+        section.modal-card-body
+          b-field(label="Name")
+            b-input(value="Thomas" required)
+          b-field(label="Number")
+            b-input(value="+1 (650) 1234" required)
+          b-field(label="Last 4 digits of SSN")
+            b-input(value="" required)
+          b-field(label="Postal Code")
+            b-input(value="94000" required)
+          b-field(label="Card Number")
+            b-input(value="" required)
+          b-field(label="CVV")
+            b-input(value="" required)
+        footer.modal-card-foot
+          button.button.is-primary(@click="submitForm") Submit
 </template>
 
 <script>
@@ -31,16 +53,37 @@ export default {
 
   data() {
     return {
-      isModalActive: false
+      isModalActive: false,
+      isModalFormActive: false
     };
   },
 
   methods: {
     selectItem(i) {
-      this.isModalActive = true;
+      if (this.merchant.items[i].insufficientCredit) {
+        this.isModalActive = true;
+      } else {
+        this.$toast.open({
+          message: `Got it! This item will be waiting for you at ${
+            this.merchant.name
+          }`,
+          type: "is-success"
+        });
+        this.$emit("route");
+      }
     },
     acceptTerms() {
       this.isModalActive = false;
+      this.isModalFormActive = true;
+    },
+    submitForm() {
+      this.isModalFormActive = false;
+      this.$toast.open({
+        message: `Got it! This item will be waiting for you at ${
+          this.merchant.name
+        }`,
+        type: "is-success"
+      });
       this.$emit("route");
     }
   }
