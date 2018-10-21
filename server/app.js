412ed8463478242e3cc465d3e51d330da3d8d35b
JunQuann <limjunquann@gmail.com>
2018-10-21 02:58:09 -0700

    included synchronyAPI

--- server/app.js
+++ server/app.js
@@ -8,11 +8,17 @@ var merchantRoute = require('./routes/merchantRoute');
 var currentLocRoute = require('./routes/currentLoc');
 var synchronyAPI = require('./apis/synchronyAPI')
 
+synchronyAPI.purchaseStats().then(function(value) {
+	console.log(value)
+})
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+	extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -20,19 +26,19 @@ app.use('/merchant', merchantRoute);
 app.use('/currentLocation', currentLocRoute);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
