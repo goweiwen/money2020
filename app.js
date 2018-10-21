ffd624cc135fe55a85fe4dfb29c83e80d60240be
JunQuann <limjunquann@gmail.com>
2018-10-21 05:22:11 -0700

    stupud3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var merchantRoute = require('./routes/merchantRoute');
 
 var app = express();
 
-app.use(cors)
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({
